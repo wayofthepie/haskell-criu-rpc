46156374ac5cdc9f1ccad6465d4ff90cb0d52161
wayofthepie <wayofthepie@gmail.com>
2017-02-07 21:17:41 +0000

    Update nix integration

--- default.nix
+++ default.nix
@@ -3,11 +3,12 @@
 }:
 mkDerivation {
   pname = "criu-rpc";
-  version = "0.0.0.1";
+  version = "0.0.2";
   src = ./.;
   libraryHaskellDepends = [
     base criu-rpc-types lens-family network process proto-lens text
     unix
   ];
+  description = "CRIU RPC client";
   license = stdenv.lib.licenses.mit;
 }
